42b6a3f7106dc48f67bf08b03d0436fa74d598e4
Raiyan109 <kraiyan109@gmail.com>
2024-08-25 14:57:03 +0600

    start time slot and end time slot created as dropdown and commented out previous one, set states for start and end and removed am and pm also

--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -1,14 +1,37 @@
 import { CalendarDays, Clock } from "lucide-react";
 import { Calendar } from "../components/ui/calendar"
 import { useEffect, useState } from "react";
-
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "../components/ui/select"
 
 // eslint-disable-next-line react/prop-types
 const BookingModal = ({ setOpenModal, facilityIdForModal }) => {
     const [date, setDate] = useState(new Date)
     const [timeSlot, setTimeSlot] = useState([])
-    const [selectedTimeSlot, setSelectedTimeSlot] = useState('')
+    const [selectedStartTimeSlot, setSelectedStartTimeSlot] = useState('')
+    const [selectedEndTimeSlot, setSelectedEndTimeSlot] = useState('')
 
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('date =>', date);
+        console.log('start time =>', selectedStartTimeSlot);
+        console.log('end time =>', selectedEndTimeSlot);
+    }
+
+    const handleStartTime = (value) => {
+        setSelectedStartTimeSlot(value)
+        // setSelectedStartTimeSlot(parseInt(value))
+    }
+    const handleEndTime = (value) => {
+        setSelectedEndTimeSlot(value)
+        // setSelectedEndTimeSlot(parseInt(value))
+    }
 
     useEffect(() => {
         getTime()
@@ -18,18 +41,22 @@ const BookingModal = ({ setOpenModal, facilityIdForModal }) => {
         const timeList = []
         for (let i = 10; i <= 12; i++) {
             timeList.push({
-                time: i + ':00 AM'
+                time: i + ':00'
+                // time: i + ':00 AM'
             })
             timeList.push({
-                time: i + ':30 AM'
+                time: i + ':30'
+                // time: i + ':30 AM'
             })
         }
         for (let i = 1; i <= 6; i++) {
             timeList.push({
-                time: i + ':00 PM'
+                time: i + ':00'
+                // time: i + ':00 PM'
             })
             timeList.push({
-                time: i + ':30 PM'
+                time: i + ':30'
+                // time: i + ':30 PM'
             })
         }
 
@@ -46,8 +73,8 @@ const BookingModal = ({ setOpenModal, facilityIdForModal }) => {
 
                 <div className="rounded-md space-y-10 flex flex-col items-center">
                     <h1 className="text-3xl md:text-5xl text-grayText text-center font-bold">Book Facility</h1>
-                    <form className="space-y-8">
-                        <div className="flex flex-col md:flex-row items-center gap-6">
+                    <form className="space-y-8" onSubmit={handleSubmit}>
+                        <div className="flex flex-col md:flex-row items-start gap-6">
                             {/* Calender */}
                             <div className="flex flex-col gap-3 items-baseline">
                                 <h2 className="flex gap-2 items-center text-grayText">
@@ -68,16 +95,37 @@ const BookingModal = ({ setOpenModal, facilityIdForModal }) => {
                                     <Clock className=" h-5 w-5" />
                                     Select Time Slot
                                 </h2>
-                                <div className="grid grid-cols-3 gap-2 border border-secondary rounded-lg p-3">
-                                    {timeSlot?.map((item, i) => (
+                                <div className="flex flex-col lg:flex-row gap-2 border border-secondary rounded-lg p-3">
+                                    {/* {timeSlot?.map((item, i) => (
                                         <h2 key={i} className={`p-2 border bg-secondary text-grayText rounded-full text-[15px] cursor-pointer  ${item.time === selectedTimeSlot && 'bg-[#d1d7e0] text-secondary'}`} onClick={() => setSelectedTimeSlot(item.time)}>{item.time}</h2>
-                                    ))}
+                                    ))} */}
+                                    <Select onValueChange={handleStartTime}>
+                                        <SelectTrigger className="w-[180px]">
+                                            <SelectValue placeholder="Start Time" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {timeSlot?.map((item, i) => (
+                                                <SelectItem key={i} value={item.time} className={`p-2 border bg-secondary text-grayText rounded-full text-[15px] cursor-pointer  ${item.time === selectedStartTimeSlot && 'bg-[#d1d7e0] text-secondary'}`} onChange={() => setSelectedStartTimeSlot(item.time)}>{item.time}</SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+                                    <Select onValueChange={handleEndTime}>
+                                        <SelectTrigger className="w-[180px]">
+                                            <SelectValue placeholder="End Time" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {timeSlot?.map((item, i) => (
+                                                <SelectItem key={i} value={item.time} className={`p-2 border bg-secondary text-grayText rounded-full text-[15px] cursor-pointer  ${item.time === selectedEndTimeSlot && 'bg-[#d1d7e0] text-secondary'}`} onClick={() => setSelectedEndTimeSlot(item.time)}>{item.time}</SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+
                                 </div>
                             </div>
                         </div>
                         <div className="flex flex-col-reverse md:flex-row items-center justify-end gap-4">
                             <button onClick={() => setOpenModal(false)} className="w-full md:w-fit bg-transparent border-2 border-accent text-accent font-bold py-2 px-5 rounded-full ease-in-out duration-100">Close</button>
-                            <button className="w-full md:w-fit bg-accent hover:bg-accent/80 text-grayText font-medium py-2 px-5 rounded-full ease-in-out duration-100">
+                            <button className="w-full md:w-fit bg-accent hover:bg-accent/80 disabled:bg-secondary disabled:cursor-not-allowed text-grayText font-medium py-2 px-5 rounded-full ease-in-out duration-100" disabled={!(date && selectedStartTimeSlot && selectedEndTimeSlot)}>
                                 Submit
                             </button>
                         </div>
