068c38c3d47588bebd0f3fd8091f2030c40dbec0
Raiyan109 <kraiyan109@gmail.com>
2024-08-28 06:48:39 +0600

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes/routes'
+import { MoveUp } from 'lucide-react'
+
 
 
 function App() {
-
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }
   return (
     <div className=''>
+      <button className='fixed bottom-3 right-5 p-3 bg-accent z-10 text-grayText rounded-full' onClick={scrollToTop}>
+        <MoveUp />
+      </button>
       <RouterProvider router={routes} />
     </div>
   )
