eac3536320144409251383419aad86bd9b9b87dc
Raiyan109 <kraiyan109@gmail.com>
2024-08-31 05:27:49 +0600

    ok

--- src/redux/baseApi.js
+++ src/redux/baseApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: 'https://sports-facility-booking-platform-be.vercel.app/api',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
 
